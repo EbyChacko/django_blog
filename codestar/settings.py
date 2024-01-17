594605904818f0098bdc7cc13c89915f249d8944
EbyChacko <cybe1990@gmail.com>
2024-01-17 16:23:24 +0000

    created the generic post view

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-w1yc%cqxv4ld&-zoq$dxmw-s6o(vamkg%9grfzseayvy^h^rru'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
